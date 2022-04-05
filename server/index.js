991ac6adc8297d5dc959579e18520e08147860f0
MuhonenN <miuhonen@gmail.com>
2022-04-05 17:22:09 +0300

    initial push

---
+++ server/index.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const cors = require('cors');
+const app = express();
+const PORT = 6969;
+const { graphqlHTTP } = require('express-graphql');
+const schema = require('./Schemas');
+
+app.use(
+    cors('/graphql'),
+    graphqlHTTP({
+        schema,
+        graphiql: true,
+    })
+);
+
+app.listen(PORT, () => {
+    console.log('Server Operational');
+});
